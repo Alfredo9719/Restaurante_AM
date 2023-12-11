8332714ca9a1350f422535563251bd3ab1050796
Alfredo GM <alfredogonzalezm265@gmail.com>
2023-12-10 22:23:31 -0600

    update components
    se agrega los platillos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css'
 import Footer from './components/Footer'
+import Section from './components/Section'
 import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-    <Header/>
-      <h1>Ejemplo de pages</h1>
-    <Footer/>  
+      <Header />
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-md-12 mb-4">
+            <h1 className='text-center'>Lo mas pedido de nuestro menu tapatio</h1>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <Section source='https://www.travelreport.mx/wp-content/uploads/2018/12/gastronom%C3%ADa-de-Guadalajara.jpg' texto='Torta Ahogada' />
+            </div>
+            <div className="col-md-4">
+              <Section source='https://www.cardamomo.news/__export/1682630698898/sites/debate/img/2023/04/27/mejores_tacos_en_guadalajara.png_242310155.png' texto='Tacos de barbacoa'/>
+            </div>
+            <div className="col-md-4">
+              <Section source='https://gourmetdemexico.com.mx/wp-content/uploads/2016/10/pozole_gdl.jpg' texto='Pozole'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+function Section({source="", texto=""}) {
+    return (
+        
+      <div className="card" style={{width: '18rem'}}>
+            <img src={source} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <p className="card-text">{texto}</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Section;
